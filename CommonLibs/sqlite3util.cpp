b2384bf12975507d895ba36ba53a818894223a9e
Kurtis Heimerl <kurtis.heimerl@rangenetworks.com>
2012-12-19 10:58:20 +0000

    Forgot these files needed to be moved
    git-svn-id: http://wush.net/svn/range/software/public/smqueue/trunk@4653 19bc5d8c-e614-43d4-8b26-e1612bc8e597

--- CommonLibs/sqlite3util.cpp
+++ CommonLibs/sqlite3util.cpp
@@ -1,26 +1,6 @@
 /*
-* Copyright 2010 Kestrel Signal Processing, Inc.
-* Copyright 2011 Range Networks, Inc.
-*
-* This software is distributed under the terms of the GNU Affero Public License.
-* See the COPYING file in the main directory for details.
-*
-* This use of this software may be subject to additional restrictions.
-* See the LEGAL file in the main directory for details.
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
-
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+* Written by David A. Burgess, Kestrel Signal Processing, Inc., 2010
+* The author disclaims copyright to this source code.
 */
 
 
@@ -55,7 +35,7 @@ int sqlite3_run_query(sqlite3* DB, sqlite3_stmt *stmt)
 		}
 	}
 	if ((src!=SQLITE_DONE) && (src!=SQLITE_ROW)) {
-		fprintf(stderr,"sqlite3_run_query failed: %s\n",sqlite3_errmsg(DB));
+		fprintf(stderr,"sqlite3_run_query failed: %s: %s\n", sqlite3_sql(stmt), sqlite3_errmsg(DB));
 	}
 	return src;
 }
@@ -160,7 +140,6 @@ bool sqlite3_command(sqlite3* DB, const char* query)
 	if (sqlite3_prepare_statement(DB,&stmt,query)) return false;
 	// Run the query.
 	int src = sqlite3_run_query(DB,stmt);
-	sqlite3_finalize(stmt);
 	return src==SQLITE_DONE;
 }
 
